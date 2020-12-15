67ccd6388235a4fc2887bde25e14a1fc3ee903be
tugot17 <tugot17@users.noreply.github.com>
2020-12-15 13:33:43 +0000

    autopep8 action fixes

--- app/helper.py
+++ app/helper.py
@@ -1,1 +1,6 @@
-my_awsome_dict = {1: "oneeesseeeeeeeeeee", 2:"twooooooooooooooooo", 3:"threeeeeeeeeeeeeeeeeeeeeeeeee", 4: "fourrrrrrrrrrrrr", 5: "fourrrrrrrrrrrrr"}
+my_awsome_dict = {
+    1: "oneeesseeeeeeeeeee",
+    2: "twooooooooooooooooo",
+    3: "threeeeeeeeeeeeeeeeeeeeeeeeee",
+    4: "fourrrrrrrrrrrrr",
+    5: "fourrrrrrrrrrrrr"}
